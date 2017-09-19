3fdcbfbcff57e6f607854d8ac76e7add3a57681e
dongjch <dongjch@yonyou.com>
2017-09-19 14:58:22 +0800

    修改项目名称 Change-Id: I3cfb9161362c36c11047763b8a7dcc3749668caa Signed-off-by: dongjch <dongjch@yonyou.com>

--- live-sdk/src/main/java/com/yonyou/live/sdk/service/LiveRemotService.java
+++ live-sdk/src/main/java/com/yonyou/live/sdk/service/LiveRemotService.java
@@ -10,16 +10,16 @@ public interface LiveRemotService {
 
 	String RESULT_JSON_KEY = "data";
 
-	@OpenAPI(httpMethod = OpenAPI.HttpMethod.POST, uriPath = "/broadcast-server/service/l/getlives", resultJsonKey = RESULT_JSON_KEY,resultErrCodeKey="",resultErrMsgKey="")
+	@OpenAPI(httpMethod = OpenAPI.HttpMethod.POST, uriPath = "/livecloud-server/service/l/getlives", resultJsonKey = RESULT_JSON_KEY,resultErrCodeKey="",resultErrMsgKey="")
 	String getLives(
 			@ParamAttr(location = ParamAttr.Location.URL, paramKey = "userId") final String userId) throws ServiceException;
 	
-	@OpenAPI(httpMethod = OpenAPI.HttpMethod.POST, uriPath = "/broadcast-server/service/l/getlive", resultJsonKey = "",resultErrCodeKey="",resultErrMsgKey="")
+	@OpenAPI(httpMethod = OpenAPI.HttpMethod.POST, uriPath = "/livecloud-server/service/l/getlive", resultJsonKey = "",resultErrCodeKey="",resultErrMsgKey="")
 	String getLive(
 			@ParamAttr(location = ParamAttr.Location.URL, paramKey = "liveid") final String liveid)
 			throws ServiceException;
 	
-	@OpenAPI(httpMethod = OpenAPI.HttpMethod.POST, uriPath = "/broadcast-server/service/l/create", resultJsonKey = "",resultErrCodeKey="",resultErrMsgKey="")
+	@OpenAPI(httpMethod = OpenAPI.HttpMethod.POST, uriPath = "/livecloud-server/service/l/create", resultJsonKey = "",resultErrCodeKey="",resultErrMsgKey="")
 	String createLiveRoom(
 			@ParamAttr(location = ParamAttr.Location.JSON_CONTENT, paramKey = "userId") final String userId,
 			@ParamAttr(location = ParamAttr.Location.JSON_CONTENT, paramKey = "nickname") final String nickname,
@@ -29,7 +29,7 @@ public interface LiveRemotService {
 			@ParamAttr(location = ParamAttr.Location.JSON_CONTENT, paramKey = "tenantId") final String tenantId)
 			throws ServiceException;
 	
-	@OpenAPI(httpMethod = OpenAPI.HttpMethod.POST, uriPath = "/broadcast-server/service/l/getliveAdmin", resultJsonKey = "",resultErrCodeKey="",resultErrMsgKey="")
+	@OpenAPI(httpMethod = OpenAPI.HttpMethod.POST, uriPath = "/livecloud-server/service/l/getliveAdmin", resultJsonKey = "",resultErrCodeKey="",resultErrMsgKey="")
 	void liveManage(
 			@ParamAttr(location = ParamAttr.Location.URL, paramKey = "userId") final String userId,
 			@ParamAttr(location = ParamAttr.Location.URL, paramKey = "tenantId") final String tenantId)
