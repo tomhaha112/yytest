76a98c004f018df03056d605c6274e6a83dce577
duchjc <duchjc@yonyou.com>
2017-09-21 15:50:43 +0800

    sdk参数

--- live-sdk/src/main/java/com/yonyou/live/sdk/service/LiveRemotService.java
+++ live-sdk/src/main/java/com/yonyou/live/sdk/service/LiveRemotService.java
@@ -12,7 +12,7 @@ public interface LiveRemotService {
 
 	@OpenAPI(httpMethod = OpenAPI.HttpMethod.POST, uriPath = "/livecloud-server/service/l/getlives", resultJsonKey = RESULT_JSON_KEY,resultErrCodeKey="",resultErrMsgKey="")
 	String getLives(
-			@ParamAttr(location = ParamAttr.Location.URL, paramKey = "userId") final String userId) throws ServiceException;
+			@ParamAttr(location = ParamAttr.Location.URL, paramKey = "zbid") final String zbid) throws ServiceException;
 	
 	@OpenAPI(httpMethod = OpenAPI.HttpMethod.POST, uriPath = "/livecloud-server/service/l/getlive", resultJsonKey = "",resultErrCodeKey="",resultErrMsgKey="")
 	String getLive(
--- live-yonyoucloud/src/main/java/com/yonyou/live/controller/LiveController.java
+++ live-yonyoucloud/src/main/java/com/yonyou/live/controller/LiveController.java
@@ -1,7 +1,5 @@
 package com.yonyou.live.controller;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
