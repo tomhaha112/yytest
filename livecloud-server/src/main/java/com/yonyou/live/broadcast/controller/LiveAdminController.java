2cc09b2de2b008f2d3041a6b992f8c144183e5e5
duchjc <duchjc@yonyou.com>
2017-09-25 11:15:57 +0800

    支持get

--- livecloud-server/src/main/java/com/yonyou/live/broadcast/controller/LiveAdminController.java
+++ livecloud-server/src/main/java/com/yonyou/live/broadcast/controller/LiveAdminController.java
@@ -72,112 +72,126 @@ public class LiveAdminController {
 		return result;
 	}
 	
-	@RequestMapping(value = "/createSubAdmin", method = RequestMethod.POST)
+	@RequestMapping(value = "/createSubAdmin", method = {RequestMethod.POST,RequestMethod.GET})
 	@ResponseBody
 	public JsonResponse createSubAdmin(@RequestParam(value="sweepcode") String sweepcode,
 			@RequestParam(value="sweepKey") String sweepKey) {
 		JsonResponse result = new JsonResponse();
-		String zbid = "";
-		String inviteUserId = "";
-		String[] arr = sweepcode.split("@");
-		if(arr.length != 2){
-			return result.failedWithReturn("参数异常");
-		}else{
-			zbid = arr[0].substring(9);
-			inviteUserId = arr[1];
-		}
-		
-		String qrcodeCacheKey =  new StringBuffer(REDIS_PREFIX).append(sweepKey).toString();
-		String cacheUser = jedisTemplate.get(qrcodeCacheKey);
-		if(StringUtils.isEmpty(cacheUser)){
-			return result.failedWithReturn("超时，请重新扫码");
-		}
-		
-		String[] cacheUserArray = cacheUser.split("&");
-		String userId = cacheUserArray[1];
-		
-		String headimgInfo = UserCenter.getUserAvatar(userId);
-		if(StringUtils.isEmpty(headimgInfo)){
-		    return result.failedWithReturn("友互通连接异常");
-		}
-		JSONObject headimgJson = JSONObject.parseObject(headimgInfo);
-		String headimg = "";
-		if(StringUtils.equals(headimgJson.getString("status"),"1")){
-			headimg = headimgJson.getString("avatar");
-		}
-		
-		String user = com.yonyou.yht.sdk.UserCenter.getUserById(userId);
-		if(StringUtils.isEmpty(user)){
-		    return result.failedWithReturn("友互通连接异常");
-		}
-		JSONObject userJSON = JSONObject.parseObject(user).getJSONObject("user");
-		if(StringUtils.isEmpty(userJSON.getString("userId"))){
-			return result.failedWithReturn("用户查询异常");
-		}
-		String nickname = userJSON.getString("userName");
-		
-		String vZanServer = PropertyUtil.getPropertyByKey("vzan.server.url");
-		String liveUrl = vZanServer + "/VZLive/AddYYLiveAdmin";
-		long currentTime = System.currentTimeMillis()/1000;
-		String sign = MD5Utils.getSign(currentTime);
-		String httpResult = "";
-		try {
-			httpResult = liveService.createSubAdmin(liveUrl, sign, currentTime, zbid, inviteUserId, userId, headimg, nickname);
-		} catch (ServiceException e) {
-			logger.error(e.getMessage());
-			return result.failedWithReturn("请求失败");
-		}
-		
-		JSONObject resultJSON = (JSONObject) JSONObject.parse(httpResult);
-		if (resultJSON.getBoolean("isok") != null && resultJSON.getBoolean("isok")) {
-			
-			//查看是否有邀请记录
-			LiveTenantEntity paramEntity = new LiveTenantEntity();
-			paramEntity.setAdminId(userId);
-			paramEntity.setLiveRoomId(zbid);
-			ServiceResult<LiveTenantEntity> existResult = liveRoomService.getLiveRoomByEntity(paramEntity);
-			if(existResult.isSuccess()){
-				//已存在 修改状态
-				LiveTenantEntity existEntity = existResult.getResult();
-				existEntity.setStatus("1");
-				ServiceResult<LiveTenantEntity> updateResult = liveRoomService.UpdateEntity(existEntity);
-				if(!updateResult.isSuccess()){
-					return result.failedWithReturn(updateResult.getMessage());
-				}
+		try{
+			String zbid = "";
+			String inviteUserId = "";
+			String[] arr = sweepcode.split("@");
+			if(arr.length != 2){
+				return result.failedWithReturn("参数异常");
 			}else{
-				//不存在 新增
-				
-				//根据邀请人的信息 填充被邀请人的信息
-				paramEntity = new LiveTenantEntity();
-				paramEntity.setAdminId(inviteUserId);
-				paramEntity.setLiveRoomId(zbid);
-				paramEntity.setStatus("1");
-				ServiceResult<LiveTenantEntity> inviteEntityResult = liveRoomService.getLiveRoomByEntity(paramEntity);
-				if(!inviteEntityResult.isSuccess()){
-					return result.failedWithReturn(inviteEntityResult.getMessage());
-				}
-				LiveTenantEntity inviteEntity = inviteEntityResult.getResult();
-				
-				LiveTenantEntity tenantEntity = new LiveTenantEntity();
-				tenantEntity.setAdminId(userId);
-				tenantEntity.setTenantId(inviteEntity.getTenantId());
-				tenantEntity.setAppcloudId(inviteEntity.getAppcloudId());
-				tenantEntity.setCreateTime(new Date());
-				tenantEntity.setInviteUserId(inviteUserId);
-				tenantEntity.setLiveRoomId(zbid);
-				liveRoomService.insertLiveRoom(tenantEntity);
+				zbid = arr[0].substring(9);
+				inviteUserId = arr[1];
 			}
 			
-			result.successWithData("data", resultJSON.get("Msg"));
-		}else{
-			if (StringUtils.isNotEmpty(resultJSON.getString("code")) && StringUtils.isNotEmpty(resultJSON.getString("Msg"))) {
-				logger.error("创建子管理员返回码:" + resultJSON.getString("code") + "\br" + "错误信息:" + resultJSON.getString("Msg"));
+			String qrcodeCacheKey =  new StringBuffer(REDIS_PREFIX).append(sweepKey).toString();
+			String cacheUser = jedisTemplate.get(qrcodeCacheKey);
+			if(StringUtils.isEmpty(cacheUser)){
+				return result.failedWithReturn("超时，请重新扫码");
 			}
-			result.failedWithReturn(resultJSON.getString("Msg"));
+			
+			String[] cacheUserArray = cacheUser.split("&");
+			String userId = cacheUserArray[1];
+			
+			
+			String headimg = getHeadimg(userId);
+			String nickname = getNickName(userId);
+			
+			String vZanServer = PropertyUtil.getPropertyByKey("vzan.server.url");
+			String liveUrl = vZanServer + "/VZLive/AddYYLiveAdmin";
+			long currentTime = System.currentTimeMillis()/1000;
+			String sign = MD5Utils.getSign(currentTime);
+			String httpResult = "";
+			try {
+				httpResult = liveService.createSubAdmin(liveUrl, sign, currentTime, zbid, inviteUserId, userId, headimg, nickname);
+			} catch (ServiceException e) {
+				logger.error(e.getMessage());
+				return result.failedWithReturn("请求失败");
+			}
+			
+			JSONObject resultJSON = (JSONObject) JSONObject.parse(httpResult);
+			if (resultJSON.getBoolean("isok") != null && resultJSON.getBoolean("isok")) {
+				
+				//查看是否有邀请记录
+				LiveTenantEntity paramEntity = new LiveTenantEntity();
+				paramEntity.setAdminId(userId);
+				paramEntity.setLiveRoomId(zbid);
+				ServiceResult<LiveTenantEntity> existResult = liveRoomService.getLiveRoomByEntity(paramEntity);
+				if(existResult.isSuccess()){
+					//已存在 修改状态
+					LiveTenantEntity existEntity = existResult.getResult();
+					existEntity.setStatus("1");
+					ServiceResult<LiveTenantEntity> updateResult = liveRoomService.UpdateEntity(existEntity);
+					if(!updateResult.isSuccess()){
+						return result.failedWithReturn(updateResult.getMessage());
+					}
+				}else{
+					//不存在 新增
+					
+					//根据邀请人的信息 填充被邀请人的信息
+					paramEntity = new LiveTenantEntity();
+					paramEntity.setAdminId(inviteUserId);
+					paramEntity.setLiveRoomId(zbid);
+					paramEntity.setStatus("1");
+					ServiceResult<LiveTenantEntity> inviteEntityResult = liveRoomService.getLiveRoomByEntity(paramEntity);
+					if(!inviteEntityResult.isSuccess()){
+						return result.failedWithReturn(inviteEntityResult.getMessage());
+					}
+					LiveTenantEntity inviteEntity = inviteEntityResult.getResult();
+					
+					LiveTenantEntity tenantEntity = new LiveTenantEntity();
+					tenantEntity.setAdminId(userId);
+					tenantEntity.setTenantId(inviteEntity.getTenantId());
+					tenantEntity.setAppcloudId(inviteEntity.getAppcloudId());
+					tenantEntity.setCreateTime(new Date());
+					tenantEntity.setInviteUserId(inviteUserId);
+					tenantEntity.setLiveRoomId(zbid);
+					liveRoomService.insertLiveRoom(tenantEntity);
+				}
+				
+				result.successWithData("data", resultJSON.get("Msg"));
+			}else{
+				if (StringUtils.isNotEmpty(resultJSON.getString("code")) && StringUtils.isNotEmpty(resultJSON.getString("Msg"))) {
+					logger.error("创建子管理员返回码:" + resultJSON.getString("code") + "\br" + "错误信息:" + resultJSON.getString("Msg"));
+				}
+				result.failedWithReturn(resultJSON.getString("Msg"));
+			}
+		} catch(Exception e){
+			result.failedWithReturn(e.getLocalizedMessage());
+			logger.error("创建子管理员失败", e);
 		}
 		return result;
 	}
 	
+	private String getNickName(String userId) {
+		String user = com.yonyou.yht.sdk.UserCenter.getUserById(userId);
+		if(StringUtils.isEmpty(user)){
+			throw new RuntimeException("友互通连接异常");
+		}
+		JSONObject userJSON = JSONObject.parseObject(user).getJSONObject("user");
+		if(StringUtils.isEmpty(userJSON.getString("userId"))){
+			throw new RuntimeException("用户查询异常");
+		}
+		return userJSON.getString("userName");
+	}
+
+	private String getHeadimg(String userId) {
+		String headimgInfo = UserCenter.getUserAvatar(userId);
+		if(StringUtils.isEmpty(headimgInfo)){
+			throw new RuntimeException("友互通连接异常");
+		}
+		JSONObject headimgJson = JSONObject.parseObject(headimgInfo);
+		if(StringUtils.equals(headimgJson.getString("status"),"1")){
+			return headimgJson.getString("avatar");
+		}else{
+			throw new RuntimeException("获取头像失败");
+		}
+	}
+
 	@RequestMapping(value = "/delSumAdmin", method = {RequestMethod.GET, RequestMethod.POST})
 	public JsonResponse delSumAdmin(HttpServletRequest request,HttpServletResponse response) throws IOException {
 		JsonResponse result = new JsonResponse();
@@ -196,6 +210,28 @@ public class LiveAdminController {
 			}
 		}
 		result.failedWithReturn("删除失败");
+		return result;
+	}
+	
+	@RequestMapping(value = "/getUserInfo", method = {RequestMethod.GET, RequestMethod.POST})
+	public JsonResponse getUserInfo(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		JsonResponse result = new JsonResponse();
+		try{
+			String userId = request.getParameter("userId");
+			if(StringUtils.isEmpty(userId)){
+				return result.failedWithReturn("参数有误");
+			}
+			JSONObject obj = new JSONObject();
+			String headimg = getHeadimg(userId);
+			String nickname = getNickName(userId);
+			obj.put("headimg", headimg);
+			obj.put("nickname", nickname);
+			
+			result.successWithData("data", obj);
+		}catch (Exception e){
+			result.failedWithReturn(e.getLocalizedMessage());
+			logger.error("获取用户信息失败", e);
+		}
 		return result;
 	}
 }
