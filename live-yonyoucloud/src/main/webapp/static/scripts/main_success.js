57b8a31328af334fc34b0d69d154bfd63aca88f4
qianxy5 <qianxy5@yonyou.com>
2017-09-26 10:26:43 +0800

    增加直播话题信息、用户名

--- live-yonyoucloud/src/main/webapp/static/scripts/main_success.js
+++ live-yonyoucloud/src/main/webapp/static/scripts/main_success.js
@@ -1,6 +1,73 @@
-var live_list_data='';//直播列表信息
+/*
+{
+    "status":1,
+    "data":"[
+    {
+        \"totalcount\":1,
+        \"banner\":\"http://j.vzan.cc//images/vzanbg/small_ind_2.png?ver=636397966118333335\",
+        \"Id\":455686,
+        \"starttime\":\"2017-09-21 16:07:34\",
+        \"sort\":1,
+        \"title\":\"sssss\",
+        \"viewcts\":27,
+        \"status\":-1
+    }
+    ]"
+}//直播列表数据
+
+{
+    "status":1,
+    "data":"{
+        \"data\":{
+                \"pushurl\":\"rtmp://pili-publish.vzan.com/vzanlive/131504476672337356?e=1508566067&token=ukF6gb319SJ-0vRruRI3Wo48W3-437u99TAw8bPn:AVev_uzkF3JGH7MmYWWx126hT8M=\",
+                \"speaker\":\"\",
+                \"rtmpurl\":\"\",
+                \"banner\":\"http://j.vzan.cc//images/vzanbg/small_ind_2.png?ver=636397966118333335\",
+                \"Id\":455686,
+                \"starttime\":\"2017-09-21 16:07:34\",
+                \"sort\":1,
+                \"title\":\"sssss\",
+                \"viewcts\":101,
+                \"introduction\":\"\",
+                \"status\":-1,
+                \"hlsurl\":\"\"
+                },
+        \"status\":1
+    }"
+}//直播话题
+
+{
+    "status":1,
+    "data":{
+        "headimg":"https://cdn.yonyoucloud.com/dev/yht/style/images/user.jpg",
+        "nickname":"董俊超"
+    }
+}//直播间用户头像与昵称
+*/
+
+
+
+/*var live_list = {"status":1,"data":"[{\"totalcount\":1,\"banner\":\"http://j.vzan.cc//images/vzanbg/small_ind_2.png?ver=636397966118333335\",\"Id\":455686,\"starttime\":\"2017-09-21 16:07:34\",\"sort\":1,\"title\":\"sssss\",\"viewcts\":92,\"status\":-1},{\"totalcount\":1,\"banner\":\"http://j.vzan.cc//images/vzanbg/small_ind_2.png?ver=636397966118333335\",\"Id\":1234567,\"starttime\":\"2017-09-21 16:07:34\",\"sort\":1,\"title\":\"sssss\",\"viewcts\":92,\"status\":-1}]"};// test
+var live_list_data = JSON.parse(live_list["data"]); 
+var live_list_status = '';
+
+var user_info = {
+    "status":1,
+    "data":{
+        "headimg":"https://cdn.yonyoucloud.com/dev/yht/style/images/user.jpg",
+        "nickname":"董俊超"
+    }
+};
+var user_name_receive = user_info["data"].nickname;
+var user_img_receive = user_info["data"].headimg;
+*/
+
+
+
+
+
+var live_list_data='';
 var live_list_status='';
-//var live_topic_data_receive='';//直播话题信息
 var tenantId = "rzfx6m98";
 $.ajax({  
     url: "http://172.20.18.241/live-yonyoucloud/live/getlives?tenantId="+tenantId+"&pageNum=1&pageSize=10",//后台提供的接口
@@ -9,7 +76,7 @@ $.ajax({
     dataType: "json", //数据类型是json型
     success: function (live_list) {   //成功时返回的data值，注意这个data是后台返回的值，上面的data是你要传给后台的值  
                 live_list_data = JSON.parse(live_list["data"]);//json对象数组
-                live_list_status = live_list["status"];//成功与否
+                live_list_status = live_list[status];//成功与否
             },  
     error: function () {   
                   
@@ -21,9 +88,28 @@ $.ajax({
 
 
 
+var userId = "4e28b4bf-f98d-4825-97d9-ac086a57d2ee";
+var user_name_receive = '';
+var user_img_receive = '';
+$.ajax({
+    url:"http://uastest.yyuap.com/livecloud_server/admin/l/getUserInfo?userId="+userId,
+    type:"get",
+    dataType:"json",
+    sunccess:function(data){
+        user_name_receive = user_info["data"].nickname;
+        user_img_receive = user_info["data"].headimg;
+    }
+})
+
+
+
+
+
+
 var app = new Vue({
     el:"#app",
     data:{
+        //tenantId:tenantId||"rzfx6m98",
         live_room_id:'',//纪录直播间id
         selectedindex:0,//标签切换标志
         btns:{
@@ -58,24 +144,40 @@ var app = new Vue({
         },
         before_click: "inherit",
         after_click:"none",
-        live_topic_data:''//纪录直播话题纪录      
+        live_topic_data:'',//纪录直播话题纪录
+        user_name : user_name_receive,//直播间用户头像与昵称
+        user_img : user_img_receive 
+        
     },
     methods:{
+        change_to_vzanmanagement:function(){
+            alert("没完成的方法 userid tenantid appid现在是写死的");
+            window.location.href = "http://172.20.18.241/livecloud_server/admin/l/getliveAdmin?userId=4e28b4bf-f98d-4825-97d9-ac086a57d2ee&tenantId=rzfx6m98&callback=callback&appId=1eee"
+        },
         choosetab:function(){
             return this.selectedindex = arguments[0];
         },
         clicktest:function(){
             this.live_room_id = arguments[0];
+            //alert(arguments[0]);
             var url = "http://172.20.18.241/live-yonyoucloud/live/getlive?liveId="+this.live_room_id;//利用直播间id拼接接口
+            var  live_topic_data_test = {"status":1,"data":"{\"data\":{\"pushurl\":\"rtmp://pili-publish.vzan.com/vzanlive/131504476672337356?e=1508566067&token=ukF6gb319SJ-0vRruRI3Wo48W3-437u99TAw8bPn:AVev_uzkF3JGH7MmYWWx126hT8M=\",\"speaker\":\"\",\"rtmpurl\":\"\",\"banner\":\"http://j.vzan.cc//images/vzanbg/small_ind_2.png?ver=636397966118333335\",\"Id\":455686,\"starttime\":\"2017-09-21 16:07:34\",\"sort\":1,\"title\":\"sssss\",\"viewcts\":101,\"introduction\":\"\",\"status\":-1,\"hlsurl\":\"\"},\"status\":1}"};
+            
+            var a = live_topic_data_test["data"];
+            this.live_topic_data = JSON.parse(a)["data"];
+            alert(this.live_topic_data);//test data 代替ajax请求数据
             $.ajax({  
                 url: url,
+                async:false,
                 type: "get",   //请求方式是get   
                 dataType: "json", //数据类型是json型
                 success: function (live_topic) {   //成功时返回的data值，注意这个data是后台返回的值，上面的data是你要传给后台的值  
-                			app.live_topic_data = JSON.parse(live_topic["data"])["data"];//直播话题数据json对象数组
+                            app.live_topic_data = JSON.parse(live_topic["data"])["data"];//json对象数组
                             //live_topic_status = live_topic[status];//成功与否
+                
                             this.before_click = "none";
-                            this.after_click = "blcok";  
+                            this.after_click = "blcok";
+                            
                         },  
                 error: function () {   
                   
@@ -86,8 +188,8 @@ var app = new Vue({
             }); 
         
         
-            this.before_click = "none";
-            this.after_click = "inherit";
+            /*this.before_click = "none";
+            this.after_click = "inherit";*/
             
         },
         checkto_before:function(){
